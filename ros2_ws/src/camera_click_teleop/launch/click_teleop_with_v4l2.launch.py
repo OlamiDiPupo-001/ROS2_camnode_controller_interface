c4d9e4735678848456e63256d2067fa2d055c2e3
Haithem201 <128013617+Haithem201@users.noreply.github.com>
2026-01-25 06:21:04 +0100

    update click

--- ros2_ws/src/camera_click_teleop/launch/click_teleop_with_v4l2.launch.py
+++ ros2_ws/src/camera_click_teleop/launch/click_teleop_with_v4l2.launch.py
@@ -2,17 +2,34 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+
+    camera_node = Node(
+        package='v4l2_camera',
+        executable='v4l2_camera_node',
+        output='screen'
+    )
+
+    click_teleop_node = Node(
+        package='camera_click_teleop',
+        executable='click_teleop_node',
+        output='screen'
+    )
+
+    turtlesim_node = Node(
+        package='turtlesim',
+        executable='turtlesim_node',
+        output='screen'
+    )
+    rviz_node = Node(
+    	package='rviz2',
+    	executable='rviz2',
+    	output='screen'
+    )
+
     return LaunchDescription([
-        Node(
-            package='v4l2_camera',
-            executable='v4l2_camera_node',
-            output='screen',
-            parameters=[{'image_size': [640, 480]}]
-        ),
-        Node(
-            package='camera_click_teleop',
-            executable='click_teleop_node',
-            output='screen'
-        ),
+        camera_node,
+        click_teleop_node,
+        turtlesim_node,
+        rviz_node
     ])
 
